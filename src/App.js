9436388a6233faaafdf5308880905a286a24d156
Alejandro Hugo Herms <hermsale@gmail.com>
2023-03-30 17:27:33 -0300

    se crearon los componentes, y se generaron las rutas de los path

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {HashRouter, Routes, Route} from 'react-router-dom'
+import { HomePage } from './HomePage';
+import { BlogPage } from './BlogPage';
+import { ProfilePage } from './ProfilePage';
+import { Menu } from './Menu';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* // este HashRouter funciona como un provider, asi como lo era el React Context  */}
+    <HashRouter>
+      <Menu />
+
+      <Routes>
+        {/* esta es la parte de nuestra pagina que será dinamica */}
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/blog' element={<BlogPage/>}/>
+        <Route path='/profile' element={<ProfilePage/>}/>
+        {/* cuando no funcione algo ira a esta ruta  */}
+        <Route path='*' element={<p>Not Found</p>}/>
+      </Routes>
+    </HashRouter>
+    </>
   );
 }
 
